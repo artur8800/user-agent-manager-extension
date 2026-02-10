a6a10e3e904802039893bc01d9f0b20020d320dc
Artur <artursviridonov@gmail.com>
2026-02-10 11:13:57 +0200

    feat: update UI components

--- src/pop-up/components/App.tsx
+++ src/pop-up/components/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import AppBar from './ui/molecules/app-bar';
+import logo from '@/assets/logo.svg';
 
 function App() {
-  return <div className="bg-white w-screen-400 min-h-screen-min max-h-screen-max p-4 relative">Hello from pop-up!</div>;
+  return (
+    <div className="bg-secondary w-screen-400 min-h-screen-min max-h-screen-max relative">
+      <AppBar logo={logo} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pop-up/components/widgets/app-ua-info/index.tsx
@@ -0,0 +1,5 @@
+function AppUaInfo() {
+  return <></>;
+}
+
+export default AppUaInfo;
