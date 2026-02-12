fcf818fbda39cfc92f6c4df7a4d51bfb55325764
Artur <artursviridonov@gmail.com>
2026-02-13 01:37:42 +0200

    feat: add base message class

---
+++ src/background-script/storage/index.ts
@@ -0,0 +1,60 @@
+import AppLogger from '@lib/logger';
+
+const STORAGE_KEY = 'userAgents';
+
+class AppStorage {
+  instance: chrome.storage.StorageArea;
+
+  constructor() {
+    this.instance = chrome.storage.local;
+  }
+
+  clearStorage() {
+    return new Promise((resolve) => {
+      this.instance.clear(() => {
+        if (chrome.runtime.lastError) {
+          AppLogger.error('Error clearing storage:', chrome.runtime.lastError);
+          resolve(false);
+        } else {
+          AppLogger.log('Storage cleared successfully.');
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  addItems(key: string, value: unknown) {
+    return new Promise((resolve) => {
+      this.instance.set({ [key]: value }, () => {
+        if (chrome.runtime.lastError) {
+          AppLogger.error('Error adding item to storage:', chrome.runtime.lastError);
+          resolve(false);
+        } else {
+          AppLogger.log(`Item added to storage: ${key} = ${value}`);
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  init<T>({ defaultData, storageKey }: { defaultData: T; storageKey?: string }) {
+    return new Promise((resolve) => {
+      this.instance.get(null, (data: { [key: string]: [T] }) => {
+        if (chrome.runtime.lastError) {
+          AppLogger.error('Error initializing storage:', chrome.runtime.lastError);
+          resolve(false);
+        } else {
+          const keyToCheck = storageKey || STORAGE_KEY;
+          if (data[keyToCheck].length <= 0) {
+            this.addItems(keyToCheck, defaultData);
+            AppLogger.log(`Storage initialized with default data under key "${keyToCheck}".`);
+          }
+          resolve(true);
+          AppLogger.log('Data already exists in storage, initialization skipped.', data[keyToCheck]);
+        }
+      });
+    });
+  }
+}
+
+export default AppStorage;
---
+++ src/pop-up/components/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import logo from '@/assets/logo.svg';
+
+import AppBar from './ui/molecules/app-bar';
+import ViewToggle from './widgets/view-toggle';
+
+function App() {
+  return (
+    <div className="bg-secondary w-screen-600 min-h-screen-min max-h-screen-max relative">
+      <AppBar logo={logo} />
+      <ViewToggle />
+    </div>
+  );
+}
+
+export default App;
