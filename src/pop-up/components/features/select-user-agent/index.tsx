f60d0408cccbf24d1b687ae060a8a4132666e912
Artur <artursviridonov@gmail.com>
2026-02-16 23:17:06 +0200

    feat: add checkbox

---
+++ src/pop-up/components/features/select-user-agent/index.tsx
@@ -0,0 +1,7 @@
+import { Checkbox } from '@/pop-up/components/ui/atoms/checkbox';
+
+function SelectUserAgent() {
+  return <Checkbox />;
+}
+
+export default SelectUserAgent;
--- src/pop-up/components/ui/molecules/app-data-table/index.tsx
+++ src/pop-up/components/ui/molecules/app-data-table/index.tsx
@@ -1,20 +1,20 @@
 import { concatStrings } from '@/lib/utils';
+import SelectUserAgent from '@/pop-up/components/features/select-user-agent';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/pop-up/components/ui/atoms/table';
 import AppTooltip from '@/pop-up/components/ui/molecules/app-tooltip';
 import UserAgentItem from '@/types/ua';
 
 function AppDataTable({ data }: { data: UserAgentItem[] }) {
-  console.log('Rendering AppDataTable with data:', data);
-
   return (
     <div className="p-4">
       <Table className="text-xs table-fixed overflow-hidden">
         <TableHeader>
           <TableRow>
+            <TableHead className="w-[40px] font-bold">-</TableHead>
             <TableHead className="w-[40px] font-bold">#</TableHead>
-            <TableHead className="w-[120px] font-bold">Browser</TableHead>
+            <TableHead className="w-[100px] font-bold">Browser</TableHead>
             <TableHead className="w-[80px] font-bold">Device</TableHead>
-            <TableHead className="w-[100px] font-bold">OS</TableHead>
+            <TableHead className="w-[80px] font-bold">OS</TableHead>
             <TableHead className="w-[290px]">User-Agent</TableHead>
           </TableRow>
         </TableHeader>
@@ -23,6 +23,9 @@ function AppDataTable({ data }: { data: UserAgentItem[] }) {
             <TableRow key={item.id}>
               <TableCell className="font-small">
                 <span>{index + 1}</span>
+              </TableCell>
+              <TableCell className="font-small">
+                <SelectUserAgent />
               </TableCell>
               <TableCell className="font-small">
                 <span>{concatStrings(item.browser?.name)}</span>
